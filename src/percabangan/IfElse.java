adff755d42c599aa4959794fefe17ed723ac2ae9
Shintanuriah18630754 <shintanrh26@gmail.com>
2020-05-03 13:42:10 +0800

    Bebas bebarang ja

---
+++ src/percabangan/IfElse.java
@@ -0,0 +1,15 @@
+package percabangan;
+
+public class IfElse {
+    public static void main(String[]args){
+        double totalBelanja = 54000;
+        double uangDidompet = 45000;
+
+        if (uangDidompet<totalBelanja){
+            System.out.println("Uang kurang , kurangi jajan anda");
+        }else{
+            System.out.println("Uang cukup , berelaan ai lah");
+
+    }
+}}
+
